317a90478a382456d4afd813c818d95e7f4f23fd
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-01-31 18:55:43 -0800

    TASK1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,19 +10,15 @@ class FIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """
-        Assigns item value to the dictionary
-        for the given key using FIFO algorithm
-        """
+        """ Assigns item value to the dictionary for the given key using FIFO algorithm """
         if key is not None and item is not None:
-            if key is not None and item is not None:
-                self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.keys.pop(0)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # If the cache is full, discard the first item (FIFO)
+                discarded_key, _ = next(iter(self.cache_data.items()))
                 del self.cache_data[discarded_key]
-                print('DISCARD: {:s}'.format(discarded_key))
+                print(f"DISCARD: {discarded_key}\n")
+
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Returns the value linked to the key in self.cache_data """
