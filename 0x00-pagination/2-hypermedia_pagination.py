d54689451d396455f0075f26971eb37a1138bc0c
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-02-02 10:48:10 -0800

    Task2**

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -5,26 +5,6 @@ import math
 from typing import List, Tuple
 
 
-def index_range(page, page_size):
-    """
-    Calculate the start and end index for a given page and page size.
-
-    Args:
-    - page (int): The current page number (1-indexed).
-    - page_size (int): The number of items per page.
-
-    Returns:
-    - tuple: A tuple containing start index and end index.
-    """
-    if page <= 0 or page_size <= 0:
-        raise ValueError("Both page and page_size must be positive integers.")
-
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-
-    return start_index, end_index
-
-
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -43,6 +23,23 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+
+    @staticmethod
+    def index_range(page, page_size):
+        """
+        Calculate the start and end index for a given page and page size.
+
+        Args:
+        - page (int): The current page number (1-indexed).
+        - page_size (int): The number of items per page.
+
+        Returns:
+        - tuple: A tuple containing start index and end index.
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+
+        return start_index, end_index
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
@@ -82,8 +79,11 @@ class Server:
         total_rows = len(self.dataset())
         total_pages = math.ceil(total_rows / page_size)
 
-        next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
+        next_page = page + 1
+
+        if self.index_range(page, page_size)[1] >= total_rows:
+            next_page = None
 
         return {
             'page_size': len(page_data),
